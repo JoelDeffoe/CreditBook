18fc3a45b6e2ab4680daf4c6ca8af0291bab0d3b
Joel Deffoe <joel.deffoe@edward-ia.ca>
2020-12-06 21:15:17 -0400

    add product controller

---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreditBook.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string Productdescrition { get; set; }
+        public string Image { get; set; }
+    }
+}
